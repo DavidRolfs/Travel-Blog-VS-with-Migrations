603eeeb74fd9fa5d64975cb8650af18dba2b38d2
david rolfs <davidrolfs15@gmail.com>
2017-08-02 11:42:13 -0700

    add view for experience index

---
+++ src/TravelBlog/Controllers/ExperiencesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TravelBlog.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TravelBlog.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        // GET: /<controller>/
+        private TravelBlogContext db = new TravelBlogContext();
+        public IActionResult Index()
+        {
+            return View(db.Experiences.Include(experiences => experiences.Location).ToList());
+        }
+    }
+}
---
+++ src/TravelBlog/Controllers/LocationsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelBlog.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TravelBlog.Controllers
+{
+    public class LocationsController : Controller
+    {
+        // GET: /<controller>/
+        private TravelBlogContext db = new TravelBlogContext();
+        public IActionResult Index()
+        {
+            return View(db.Locations.ToList());
+        }
+    }
+}
